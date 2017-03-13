3cc5c6277b8b04c94dad09cdc9b534fd87d405da
Gautam Singh <gksinghiet@gmail.com>
2017-03-13 14:15:52 -0500

    Add javadoc

--- src/main/java/com/livemykarma/gc/finalizer/HeavyFinalizable.java
+++ src/main/java/com/livemykarma/gc/finalizer/HeavyFinalizable.java
@@ -3,6 +3,12 @@ package com.livemykarma.gc.finalizer;
 import java.io.InputStream;
 import java.util.HashMap;
 
+/**
+ * Finalizable class with big map. Should be normalized. 
+ * 
+ * @author Gautam
+ *
+ */
 public class HeavyFinalizable {
 	HashMap<String, String> bigHashMap;
 	InputStream stream;
--- src/main/java/com/livemykarma/gc/finalizer/NormalizedFinalizer.java
+++ src/main/java/com/livemykarma/gc/finalizer/NormalizedFinalizer.java
@@ -3,7 +3,11 @@ package com.livemykarma.gc.finalizer;
 import java.io.InputStream;
 import java.util.HashMap;
 
-
+/**
+ * Normalize HeavyFinalizable to reduce data overhead
+ * @author Gautam
+ *
+ */
 public class NormalizedFinalizer {
 	HashMap<String, String> bigHashMap;
 	InputStream stream;
